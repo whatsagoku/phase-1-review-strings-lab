e90fd34f14f24a9dbee56202d988181a0f932363
Jeffery Hutchinson <jefferyshutchinson@gmail.com>
2023-03-18 17:06:22 -0400

    Welcome to Flatbook, everybody :P

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+let currentUser = "Jeffery Hutchinson"
+if (typeof currentUser !== "string") {   
+}
+
+console.log(currentUser);
+
+let welcomeMessage = "Welcome to Flatbook, Jeffery Hutchinson!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, JEFFERY HUTCHINSON!"
+
+let shortGreeting = "Welcome, J!"
